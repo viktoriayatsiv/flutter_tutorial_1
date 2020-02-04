d2b720588050ce1b9ffeff13430e78b8e419eb58
vyatsi <vyatsi@softserveinc.com>
2020-02-04 19:17:14 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Hello world!'),
+          ),
+          body: ProductManager('Food start'),
+        ));
+  }
+}
